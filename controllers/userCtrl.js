7b94c50efa033d5ad573e4b541cffb577695e847
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2021-12-15 12:40:35 +0000

    create access/refreh token

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,4 +1,6 @@
 const Users = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userCtrl = {
   register: async (req, res) => {
@@ -14,11 +16,54 @@ const userCtrl = {
           .status(400)
           .json({ msg: 'Password is atleast 6 characters long' });
 
-      res.json({ msg: 'Register Success!' });
+      // Password Encryption
+      const passwordHash = await bcrypt.hash(password, 10);
+      const newUser = new Users({
+        name,
+        email,
+        password: passwordHash,
+      });
+
+      // Save mongodb
+      await newUser.save();
+
+      // Then create jsonwebtoken to authentication
+      const accesstoken = createAccessToken({ id: newUser._id });
+      const refreshtoken = createRefreshToken({ id: newUser._id });
+
+      res.cookie('refreshtoken', refreshtoken, {
+        // httpOnly: true,
+        path: '/user/refresh_token',
+      });
+
+      res.json({ accesstoken });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
+  refreshToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token)
+        return res.status(400).json({ msg: 'Please Login or Register' });
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(400).json({ msg: 'Please Login or Register' });
+
+        const accesstoken = createAccessToken({ id: user.id });
+
+        res.json({ accesstoken });
+      });
+    } catch (err) {}
+  },
+};
+
+const createAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+};
+const createRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 };
 
 module.exports = userCtrl;

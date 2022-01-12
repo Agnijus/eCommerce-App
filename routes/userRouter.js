7754f38ea80482691789d46fb58026f7cde5e615
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-01-12 18:43:01 +0000

    logout request to refresh token

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,8 @@ router.post('/register', userCtrl.register);
 
 router.post('/login', userCtrl.login);
 
+router.get('/logout', userCtrl.logout);
+
 router.get('/refresh_token', userCtrl.refreshToken);
 
 module.exports = router;

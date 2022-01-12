21c7d2704c9dae68faea02089320f2246ee1b051
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-01-12 18:14:07 +0000

    check user login info

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -41,6 +41,19 @@ const userCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await Users.findOne({ email });
+      if (!user) return res.status(400).json({ msg: 'User does not exist>' });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: 'Incorrect password.' });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
   refreshToken: (req, res) => {
     try {
       const rf_token = req.cookies.refreshtoken;

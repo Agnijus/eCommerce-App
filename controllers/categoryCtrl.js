7ceb929ef75c7c45b1b107159670310b175a4b0a
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-01-19 21:17:20 +0000

    update category request

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -32,5 +32,15 @@ const categoryCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  updateCategory: async (req, res) => {
+    try {
+      const { name } = req.body;
+      await Category.findOneAndUpdate({ _id: req.params.id }, { name });
+
+      res.json({ msg: 'Updated a category' });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 module.exports = categoryCtrl;

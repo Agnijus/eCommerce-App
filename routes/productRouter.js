126bf656921c5f22678773b4d5447411815efd3a
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-01-24 22:36:08 +0000

    getProduct and createProduct initialization

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const productCtrl = require('../controllers/productCtrl');
 
-router.route('/products').get().post();
+router
+  .route('/products')
+  .get(productCtrl.getProduct)
+  .post(productCtrl.createProduct);
 
-router.route('/products/:id').delete().put();
+router
+  .route('/products/:id')
+  .delete(productCtrl.deleteProduct)
+  .put(productCtrl.updateProduct);
 
 module.exports = router;

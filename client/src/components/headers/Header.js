601889a5b203aca1cacadbb28417368187ae7ead
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-01-31 23:43:39 +0000

    feat(header): icon init

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,9 +1,41 @@
 import React, { useState, useContext } from 'react';
 import { GlobalState } from '../../GlobalState';
+import Menu from './icon/menu.svg';
+import Close from './icon/close.svg';
+import Cart from './icon/cart.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const value = useContext(GlobalState);
-  return <div>{value}</div>;
+  return (
+    <header>
+      <div className='menu'>
+        <img src={Menu} alt='' width='30px' />
+      </div>
+      <div className='logo'>
+        <h1>
+          <Link to='/'>Shop</Link>
+        </h1>
+      </div>
+      <ul>
+        <li>
+          <Link to='/'>Products</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login Register</Link>
+        </li>
+        <li>
+          <img src={Close} alt='' width='30px' />
+        </li>
+      </ul>
+      <div className='cart-icon'>
+        <span>0</span>
+        <Link to='/'>
+          <img src={Cart} alt='' width='30px' />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

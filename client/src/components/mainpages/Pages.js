478a1cfe5610957540c64e687e6d592fd8ed2bfb
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-02-03 23:41:07 +0000

    feat(mainpages): NotFound page route

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -4,6 +4,7 @@ import Products from './products/Products';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import Cart from './cart/Cart';
+import NotFound from './utils/NotFound/NotFound';
 
 const Pages = () => {
   return (
@@ -12,6 +13,7 @@ const Pages = () => {
       <Route path='/login' component={Login} />
       <Route path='/register' component={Register} />
       <Route path='/cart' component={<Cart />} />
+      <Route path='*' component={<NotFound />} />
     </Routes>
   );
 };

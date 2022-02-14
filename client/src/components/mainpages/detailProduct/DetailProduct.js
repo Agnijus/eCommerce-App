b92622639bbda51f415d03be35326085020dba65
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-02-14 22:24:45 +0000

    feat(DetailProduct): display detailProduct

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { GlobalState } from '../../../GlobalState';
 
 const DetailProduct = () => {
@@ -17,7 +17,25 @@ const DetailProduct = () => {
   }, [params, products]);
   console.log(detailProduct);
 
-  return <div className='detail'></div>;
+  if (detailProduct.length === 0) return null;
+  return (
+    <div className='detail'>
+      <img src={detailProduct.images.url} alt='' />
+      <div className='box-detail'>
+        <div className='row'>
+          <h2>{detailProduct.title}</h2>
+          <h6>{detailProduct.product_id}</h6>
+        </div>
+        <span>{detailProduct.price}£</span>
+        <p>{detailProduct.description}</p>
+        <p>{detailProduct.content}</p>
+        <p>Sold: {detailProduct.sold}</p>
+        <Link to='/cart' className='cart'>
+          Buy Now
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default DetailProduct;

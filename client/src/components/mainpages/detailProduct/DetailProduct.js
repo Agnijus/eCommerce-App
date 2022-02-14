b5dbd58980e9c0ab80fa980f3e0e98c7f3c6b3f1
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-02-14 22:18:54 +0000

    feat(DetailProduct): getProduct by id

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { GlobalState } from '../../../GlobalState';
 
 const DetailProduct = () => {
-  return <div>DetailProduct</div>;
+  const params = useParams();
+  const state = useContext(GlobalState);
+  const [products] = state.ProductsAPI.products;
+  const [detailProduct, setDetailProduct] = useState([]);
+
+  useEffect(() => {
+    if (params) {
+      products.forEach((product) => {
+        if (product._id === params.id) setDetailProduct(product);
+      });
+    }
+  }, [params, products]);
+  console.log(detailProduct);
+
+  return <div className='detail'></div>;
 };
 
 export default DetailProduct;

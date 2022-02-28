d8512b19bfb27ae25cc505f49d6726c88c053655
Agnijus Botyrius <agnijus.botyrius01@gmail.com>
2022-02-28 05:49:29 +0000

    feat(DetailProduct): map related products by category

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { GlobalState } from '../../../GlobalState';
+import ProductItem from '../utils/ProductItem';
 
 const DetailProduct = () => {
   const params = useParams();
@@ -19,22 +20,34 @@ const DetailProduct = () => {
 
   if (detailProduct.length === 0) return null;
   return (
-    <div className='detail'>
-      <img src={detailProduct.images.url} alt='' />
-      <div className='box-detail'>
-        <div className='row'>
-          <h2>{detailProduct.title}</h2>
-          <h6>id: {detailProduct.product_id}</h6>
+    <>
+      <div className='detail'>
+        <img src={detailProduct.images.url} alt='' />
+        <div className='box-detail'>
+          <div className='row'>
+            <h2>{detailProduct.title}</h2>
+            <h6>id: {detailProduct.product_id}</h6>
+          </div>
+          <span>{detailProduct.price}£</span>
+          <p>{detailProduct.description}</p>
+          <p>{detailProduct.content}</p>
+          <p>Sold: {detailProduct.sold}</p>
+          <Link to='/cart' className='cart'>
+            Buy Now
+          </Link>
         </div>
-        <span>{detailProduct.price}£</span>
-        <p>{detailProduct.description}</p>
-        <p>{detailProduct.content}</p>
-        <p>Sold: {detailProduct.sold}</p>
-        <Link to='/cart' className='cart'>
-          Buy Now
-        </Link>
       </div>
-    </div>
+      <div>
+        <h2>Related products</h2>
+        <div>
+          {products.map((product) => {
+            return product.category === detailProduct.category ? (
+              <ProductItem key={product._id} product={product} />
+            ) : null;
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
